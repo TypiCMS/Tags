2250a0e433f9f9fc5cda4ca3003428be73eca818
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 16:45:11 +0100

    admin controller docblocks

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Tags\Http\Controllers;
 
 use TypiCMS\Modules\Core\Http\Controllers\BaseAdminController;
 use TypiCMS\Modules\Tags\Http\Requests\FormRequest;
+use TypiCMS\Modules\Tags\Models\Tag;
 use TypiCMS\Modules\Tags\Repositories\TagInterface;
 
 class AdminController extends BaseAdminController
@@ -16,29 +17,29 @@ class AdminController extends BaseAdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Tags\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(FormRequest $request)
     {
-        $model = $this->repository->create($request->all());
+        $tag = $this->repository->create($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $tag);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  $model
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Tags\Models\Tag                $model
+     * @param \TypiCMS\Modules\Tags\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update($model, FormRequest $request)
+    public function update(Tag $tag, FormRequest $request)
     {
         $this->repository->update($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $tag);
     }
 }
