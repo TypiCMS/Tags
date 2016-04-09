c4afcce5c2df04731a2762258c6fae9a11060a21
Samuel De Backer <sdebacker@gmail.com>
2016-04-09 13:45:16 +0200

    $module removed

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,14 +1,12 @@
 @extends('core::admin.master')
 
-@section('title', trans($module.'::global.name'))
+@section('title', trans('tags::global.name'))
 
 @section('main')
 
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
-    <a href="{{ route('admin::create-'.str_singular($module)) }}" class="btn-add" title="@lang($module.'::global.New')">
-        <i class="fa fa-plus-circle"></i><span class="sr-only">@lang($module.'::global.New')</span>
-    </a>
+    @include('core::admin._button-create', ['module' => 'tags'])
 
     <h1>
         <span>@{{ models.length }} @choice('tags::global.tags', 2)</span>
@@ -37,7 +35,7 @@
                 <tr ng-repeat="model in displayedModels">
                     <td typi-btn-delete action="delete(model, model.tag)"></td>
                     <td>
-                        @include('core::admin._button-edit')
+                        @include('core::admin._button-edit', ['module' => 'tags'])
                     </td>
                     <td>@{{ model.tag }}</td>
                     <td>@{{ model.uses }}</td>
