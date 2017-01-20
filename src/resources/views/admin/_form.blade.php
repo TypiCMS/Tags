9bfe6283fc3ed40eb976df9cafbc3809c8247017
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:23:01 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -4,14 +4,14 @@
 
 <div class="row">
     <div class="col-md-6">
-        {!! BootForm::text(__('validation.attributes.tag'), 'tag') !!}
+        {!! BootForm::text(__('Tag'), 'tag') !!}
     </div>
     <div class="col-md-6 form-group @if($errors->has('slug'))has-error @endif">
-        {!! Form::label(__('validation.attributes.slug'))->addClass('control-label')->forId('slug') !!}
+        {!! Form::label(__('Slug'))->addClass('control-label')->forId('slug') !!}
         <div class="input-group">
             {!! Form::text('slug')->addClass('form-control')->id('slug')->data('slug', 'tag') !!}
             <span class="input-group-btn">
-                <button class="btn btn-default btn-slug @if($errors->has('slug'))btn-danger @endif" type="button">@lang('validation.attributes.generate')</button>
+                <button class="btn btn-default btn-slug @if($errors->has('slug'))btn-danger @endif" type="button">{{ __('Generate') }}</button>
             </span>
         </div>
         {!! $errors->first('slug', '<p class="help-block">:message</p>') !!}
