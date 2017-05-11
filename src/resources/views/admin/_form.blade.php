124822e7d8ba92c3becc2f89dabde389833e5d7d
Samuel De Backer <sdebacker@gmail.com>
2017-05-11 21:18:21 +0200

    html5 required field

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -5,12 +5,12 @@
 
 <div class="row">
     <div class="col-md-6">
-        {!! BootForm::text(__('Tag'), 'tag') !!}
+        {!! BootForm::text(__('Tag'), 'tag')->required() !!}
     </div>
     <div class="col-md-6 form-group @if($errors->has('slug'))has-error @endif">
         {!! Form::label(__('Slug'))->addClass('control-label')->forId('slug') !!}
         <div class="input-group">
-            {!! Form::text('slug')->addClass('form-control')->id('slug')->data('slug', 'tag') !!}
+            {!! Form::text('slug')->addClass('form-control')->id('slug')->data('slug', 'tag')->required() !!}
             <span class="input-group-btn">
                 <button class="btn btn-default btn-slug @if($errors->has('slug'))btn-danger @endif" type="button">{{ __('Generate') }}</button>
             </span>
