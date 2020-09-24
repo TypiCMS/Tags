70debda0c8511f5523b66f734931e1f8902da8ce
Samuel De Backer <sdebacker@gmail.com>
2020-09-24 17:33:30 +0200

    default markup

--- src/resources/views/public/index.blade.php
+++ src/resources/views/public/index.blade.php
@@ -2,18 +2,26 @@
 
 @section('bodyClass', 'body-tags body-tags-index body-page body-page-'.$page->id)
 
-@section('content')
+@section('page')
 
-    <div class="rich-content">{!! $page->present()->body !!}</div>
+<div class="page-body">
 
-    @include('files::public._documents', ['model' => $page])
-    @include('files::public._images', ['model' => $page])
+    <div class="page-body-container">
+
+        <div class="rich-content">{!! $page->present()->body !!}</div>
+
+        @include('files::public._documents', ['model' => $page])
+        @include('files::public._images', ['model' => $page])
 
 
-    @if ($models->count() > 0)
-    @include('tags::public._list', ['items' => $models])
-    @endif
+        @if ($models->count() > 0)
+        @include('tags::public._list', ['items' => $models])
+        @endif
 
-    {!! $models->appends(Request::except('page'))->links() !!}
+        {!! $models->appends(Request::except('page'))->links() !!}
+
+    </div>
+
+</div>
 
 @endsection
