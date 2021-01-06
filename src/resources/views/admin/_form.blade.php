2287e907323095545c3e4c9348b8f375cb3567b2
Samuel De Backer <sdebacker@gmail.com>
2021-01-06 13:18:07 +0100

    BS5

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -8,12 +8,10 @@
         {!! BootForm::text(__('Tag'), 'tag')->required() !!}
     </div>
     <div class="col-md-6 mb-3 @if ($errors->has('slug'))has-error @endif">
-        {!! Form::label(__('Slug'))->addClass('control-label')->forId('slug') !!}
+        {!! Form::label(__('Slug'))->addClass('form-label')->forId('slug') !!}
         <div class="input-group">
             {!! Form::text('slug')->addClass('form-control')->addClass($errors->has('slug') ? 'is-invalid' : '')->id('slug')->data('slug', 'tag') !!}
-            <span class="input-group-append">
-                <button class="btn btn-outline-secondary btn-slug @if ($errors->has('slug'))btn-danger @endif" type="button">{{ __('Generate') }}</button>
-            </span>
+            <button class="btn btn-outline-secondary btn-slug @if ($errors->has('slug'))btn-danger @endif" type="button">{{ __('Generate') }}</button>
             {!! $errors->first('slug', '<div class="invalid-feedback">:message</div>') !!}
         </div>
     </div>
